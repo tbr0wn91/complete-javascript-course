a699264d4bd4d71673c30cb688dd6ee46089048d
thomas brown <thomasbrown9101@gmail.com>
2019-10-08 12:04:17 -0700

    finished scope

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -2,32 +2,32 @@
 // Lecture: Hoisting
 
 
-calculateAge(1990);
+// calculateAge(1990);
 
-function calculateAge(year){
-    console.log(2016 - year);
-}
+// function calculateAge(year){
+//     console.log(2016 - year);
+// }
 
-// retirement(1990);
+// // retirement(1990);
 
-var retirement = function(year){
-    console.log(65 - (2016 - year));
-}
+// var retirement = function(year){
+//     console.log(65 - (2016 - year));
+// }
 
 
-//variables
+// //variables
 
-console.log(age)
-var age = 23;
-console.log(age);
+// console.log(age)
+// var age = 23;
+// console.log(age);
 
-function foo() {
-    var age = 65;
-    console.log(age)
-}
+// function foo() {
+//     var age = 65;
+//     console.log(age)
+// }
 
-foo();
-console.log(age);
+// foo();
+// console.log(age);
 
 
 
@@ -46,20 +46,20 @@ console.log(age);
 
 // First scoping example
 
-/*
-var a = 'Hello!';
-first();
 
-function first() {
-    var b = 'Hi!';
-    second();
+// var a = 'Hello!';
+// first();
 
-    function second() {
-        var c = 'Hey!';
-        console.log(a + b + c);
-    }
-}
-*/
+// function first() {
+//     var b = 'Hi!';
+//     second();
+
+//     function second() {
+//         var c = 'Hey!';
+//         console.log(a + b + c);
+//     }
+// }
+
 
 
 
