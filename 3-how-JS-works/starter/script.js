ed87700ab4155e50e5e77eb633ffc5260505946e
thomas brown <thomasbrown9101@gmail.com>
2019-10-08 09:00:46 -0700

    finished hoisting

--- 3-how-JS-works/starter/script.js
+++ 3-how-JS-works/starter/script.js
@@ -2,9 +2,32 @@
 // Lecture: Hoisting
 
 
+calculateAge(1990);
+
+function calculateAge(year){
+    console.log(2016 - year);
+}
+
+// retirement(1990);
+
+var retirement = function(year){
+    console.log(65 - (2016 - year));
+}
 
 
+//variables
 
+console.log(age)
+var age = 23;
+console.log(age);
+
+function foo() {
+    var age = 65;
+    console.log(age)
+}
+
+foo();
+console.log(age);
 
 
 
