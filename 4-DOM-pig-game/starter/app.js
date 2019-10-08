b309f05af919ddbff773b0fda95a4b3c4e993156
thomas brown <thomasbrown9101@gmail.com>
2019-10-08 16:04:59 -0700

    started game

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,17 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+
+document.querySelector('.dice').style.display = 'none';
